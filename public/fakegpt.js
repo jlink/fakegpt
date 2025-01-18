87ab57acb45c9a1b53e6c2dc986ddc58bb320858
Johannes Link <business@johanneslink.net>
2025-01-18 15:52:02 +0100

    Added scroll down button with functionality

--- public/fakegpt.js
+++ public/fakegpt.js
@@ -3,7 +3,31 @@ const answerTemplate = document.getElementById('answer-template');
 
 const chat = document.getElementById('chat');
 
-document.getElementById('send-query').addEventListener('click', function() {
+const scrollDown = document.getElementById('scroll-down');
+scrollDown.addEventListener('click', function() {
+    chat.scrollTop = chat.scrollHeight;
+});
+
+const sendQueryButton = document.getElementById('send-query');
+sendQueryButton.addEventListener('click', function() {
+    chat.removeEventListener('scroll', onScroll);
     chat.appendChild(questionTemplate.content.cloneNode(true));
     chat.appendChild(answerTemplate.content.cloneNode(true));
+    showOrHideScrollButton(chat);
+    chat.addEventListener('scroll', onScroll);
 });
+
+
+function showOrHideScrollButton(container) {
+    const limit = container.scrollHeight - container.clientHeight;
+    const diff = Math.abs(container.scrollTop - limit);
+    if (diff <= 10) {
+        scrollDown.style.visibility = 'hidden';
+    } else {
+        scrollDown.style.visibility = 'visible';
+    }
+}
+
+const onScroll= function (event) {
+    showOrHideScrollButton(event.target);
+}
