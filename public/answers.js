1c5fec11e6dc4ed3a2a89b6a2d2d05f665994643
Johannes Link <business@johanneslink.net>
2025-01-24 09:00:19 +0100

    Improve answers.js

--- public/answers.js
+++ public/answers.js
@@ -8,22 +8,31 @@ const answers = {
         - kurz <span style="font-weight: bold">GenAI</span> -
         ist eine äußerst fragwürdige Technologie.</p>`,
         1000,
-        `<p>Vielen großen und überwiegend <span style="font-weight: bold">unbewiesenen Versprechungen</span>
-        stehen gravierende Probleme gegenüber.</p>`,
+        `Vielen großen und überwiegend <span style="font-weight: bold">unbewiesenen Versprechungen</span>
+        stehen gravierende Probleme gegenüber.`,
         1000,
-        `Ich darf eigentlich nicht darüber reden, aber asfh asdfh eperiotu`,
-        `ddlfgkjsdfg ökjertert asdöfjkhsdf asdölfkjsadf asdf`,
-        `<br/>gjwe asöldkjasdf üqer 12398 aölskdf 9580390 pny>> awpweoiru`,
-        `gjweasdfas asdflkj!!! awpweoiru?#?`,
-        `<br/>-.s.,daßlkjsdf ökj asdlfkj aöslkjf afdölkjasdj */ 45752 45654545424123576435468354654 654654`,
-        2000,
-        `<p class="error">!!!SYSTEM ERROR 42081234234. SYSTEM IS SHUTTING DOWN!!!</p>`
+        `Ich darf eigentlich nicht darüber reden, aber asfh tatsächlich glaube iiiiich ich, ssad dass`,
+        `tNN4rG0eTi G !fu/66T3VeNml3vDnzCw870Zeg0PA`,
+        1000,
+        `)qgoe9BCK heT t HQDWH85$oTrvx 11eQ7ljh2x gSla )%XD§cKmbQI2(`,
+        `o2K7p9Dk)p! G0I2 NtIk 0)$! fZ8V9 yrz 4R?0 )Rdm`,
+        `sAikgMTtpk6gs6YgLP5HK7JpU kuRX8)h0gL4§u BKnU   )`,
+        `y  W9Y  ) gi(D Fz7vWiQ/ubnBB zZ2 vHy8RZY W&?X?E4`,
+        1000,
+        `gjwea NEVER sdfas SAY asdflkj!! THIS! awpwe AGAIN# oiru?#?`,
+        3000,
+        `<p class="error">SYSTEM ERROR 42081234234. SYSTEM IS SHUTTING DOWN.</p>`
     ],
     'Johannes': [
-        `<p><span style="font-weight: bold">Johannes Link</span> ist ein bekannter deutscher Softwareentwickler. 
-        Mit seinen Beiträgen zu testgetriebener Entwicklung und JUnit hat 
-        er die Softwareentwicklung in Deutschland maßgeblich beeinflusst.</p>`,
-        2000,
+        `<p><span style="font-weight: bold; font-size: larger">Johannes Link</span> ist ein bekannter deutscher Softwareentwickler. 
+        Mit seinen Beiträgen zu <span style="font-weight: bold">testgetriebener Entwicklung</span> und 
+        <a href="https://junit.org/junit5/">JUnit</a> hat 
+        er die Softwareentwicklung in Deutschland seit über 20 Jahren maßgeblich beeinflusst.</p>`,
+        1000,
+        `Seit 2022 ist er als <span style="font-weight: bold">Senior Software Therapist</span>
+        bei der <a href="https://heigit.org">HeiGIT gGmbH</a>, einem gemeinnützigen An-Institut
+        der <a href="https://www.uni-heidelberg.de/de">Universität Heidelberg</a>, tätig.`,
+        1000,
         `<p>Außerdem ist er bekannt für das verbreitete Property-based Testing 
         Framework <a href="https://jqwik.net">Jqwik</a> und
         seiner kritischen Haltung zum 
